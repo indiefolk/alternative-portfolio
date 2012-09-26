ee9092cb69ae081b707b519738c1559a26a88ed4
John Lund <john.lund@piratalondon.com>
2012-09-26 17:29:05 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// $(document).ready(function () {
+
+// 	//var $domi = $('#container').oriDomi();
+
+
+// 	var $domi = $('#container').oriDomi({ vPanels: 3, hPanels: 10,
+//         perspective: 500, speed: 500, shading: true });
+
+
+// 	var clicked = false;
+
+
+// 	$('#container').on('click', function (e) {
+
+// 		if (clicked) {
+// 			$domi.oriDomi('unfold', -30, 'right');
+// 			clicked = false;
+
+// 		}
+// 		else {
+// 			$domi.oriDomi('foldUp', 0, 'right');
+// 			clicked = true;
+// 		}
+
+// 	});
+
+// });
